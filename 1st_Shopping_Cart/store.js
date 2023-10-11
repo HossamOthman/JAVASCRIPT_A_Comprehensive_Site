cfc6a01672730cbde1d60b35d4a480c22d41c15a
Hossam Othman <rowheadrex@gmail.com>
2023-10-11 21:18:32 +0200

    add items to cart

--- 1st_Shopping_Cart/store.js
+++ 1st_Shopping_Cart/store.js
@@ -20,6 +20,13 @@ function ready() {
         var input = quantityInputs[i];
         input.addEventListener('change', quantityChanged)
     }
+
+    var addToCartBtns = document.getElementsByClassName('shop-item-button');
+    for (var i = 0; i < addToCartBtns.length; i++) {
+        var button = addToCartBtns[i];
+        button.addEventListener('click', addToCartClicked)
+    }
+
 }
 
 
@@ -54,5 +61,46 @@ function quantityChanged(e) {
     if(isNaN(input.value) || input.value <= 0) {
         input.value = 1
     }
-    updateCartTotal()
+    updateCartTotal();
+}
+
+function addToCartClicked(e) {
+    var button = e.target;
+    var shopItem = button.parentElement.parentElement;
+    var title = shopItem.querySelector('.shop-item-title').innerText;
+    var price = shopItem.querySelector('.shop-item-price').innerText;
+    var imgSrc = shopItem.querySelector('.shop-item-image').src;
+    
+    addItemToCart(title, price, imgSrc);
+    updateCartTotal();
+}
+
+function addItemToCart(title, price, imgSrc) {
+    var cartRow = document.createElement('div'); 
+    cartRow.className = 'cart-row';
+    var cartItems = document.querySelector('.cart-items');
+
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title');
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if(cartItemNames[i].innerText == title) {
+            alert('items exists in the cart')
+            return;
+        }
+    }
+    var cartRowContent = `
+            <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imgSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+            </div>
+            <span class="cart-price cart-column">${price}</span>
+            <div class="cart-quantity cart-column">
+                <input class="cart-quantity-input" type="number" value="1">
+                <button class="btn btn-danger" type="button">REMOVE</button>
+            </div>
+    `
+    
+    cartRow.innerHTML = cartRowContent;
+    cartItems.append(cartRow);
+    cartRow.querySelector('.btn-danger').addEventListener('click', removeCartItem);
+    cartRow.querySelector('.cart-quantity-input').addEventListener('change', quantityChanged);
 }
