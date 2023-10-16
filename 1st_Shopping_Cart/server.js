c1075e9ab8450095c2ae5cc07b19fd3dff3ef708
Hossam Othman <rowheadrex@gmail.com>
2023-10-16 16:55:08 +0200

    store.ejs & router for it

--- 1st_Shopping_Cart/server.js
+++ 1st_Shopping_Cart/server.js
@@ -4,12 +4,28 @@ if(process.env.NODE_ENV !== 'production') {
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
 
-console.log(stripeSecretKey)
+
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
+
+app.get('/store', function(req, res) {
+    fs.readFile('items.json', (err, data) => {
+        if (err) {
+            res.status(500).end();
+        } else  {
+            res.render('store.ejs', {
+                items : JSON.parse(data)
+            })
+        }
+    })
+})
+
+
+
 
 app.listen(3000)
 
